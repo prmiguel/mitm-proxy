5871c8b30064f5a37bc385d40a4fa191dfaa15a1
Miguel A. Pari Rojas <miangel-pr@outlook.com>
2024-03-10 08:02:10 -0400

    Wait for proxy server in env var

--- desktop/scripts/check_proxy_server.sh
+++ desktop/scripts/check_proxy_server.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-sleep 30
+wait_proxy_server_sec="30"
+if [ -n "${WAIT_PROXY_SERVER}" ]; then
+    wait_proxy_server_sec="${WAIT_PROXY_SERVER}"
+fi
+echo "Wait for PROXY_SERVER [$wait_proxy_server_sec]"
+sleep "$wait_proxy_server_sec"
